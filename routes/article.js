a78709234a429db567946eaec71966813b97ee10
Batamar1 <metema7@gmail.com>
2017-06-05 12:32:08 +0300

    add news

--- routes/article.js
+++ routes/article.js
@@ -1,4 +1,11 @@
 const router = require('express').Router();
+const fileUpload = require('express-fileupload');
+const bodyParser = require("body-parser");
+
+const mysql = require(__basePath + '/modules/mysql.js');
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+router.use(fileUpload());
 
 router.get('/', (req, res) => {
   res.render('article/article', {
@@ -12,6 +19,20 @@ router.get('/new', (req, res) => {
       session: req.session
     });
   } else res.redirect('/');
+});
+
+
+router.post('/new',urlencodedParser,(req, res) => {
+  mysql.query('INSERT INTO `news` VALUES (NULL, ?, ?, ?);', [req.body.title, req.body.desc,0], 
+  function(err, result){
+    mysql.query('SELECT `id` FROM `news` ORDER BY `news`.`id` DESC LIMIT 0, 1;', function(err, result){
+      console.log(result);
+      req.files.image.mv(__basePath + '/public/user_files/news/' + result[0].id, function(err) {
+        if(err) console.log(err);
+      });
+      res.redirect("/");
+    });
+  });
 });
 
 module.exports = router;
